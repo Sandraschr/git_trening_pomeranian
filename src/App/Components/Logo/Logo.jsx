8bb66e829fbbb02a52f878320ac16d974d1c8833
Sandra <sandra@example.com>
2023-07-12 08:53:45 +0200

    html1

---
+++ src/App/Components/Logo/Logo.jsx
@@ -0,0 +1,9 @@
+import { ReactComponent as LogoSVG } from '../../Images/start-it-logo.svg';
+
+
+export const Logo = () => {
+return <div>
+
+    <LogoSVG />
+</div>;
+}; 
---
+++ src/App/Exercises/HtmlCss/GoogleFonts/router-data.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+import { GoogleFonts } from './index';
+
+export const blockRouterMetaData = {
+
+  path: 'GoogleFonts',
+
+  date: '11-07-2023',
+
+  linkLabel: 'Google Fonts',
+
+  blockNo: 11,
+
+  element: <GoogleFonts />,
+
+  tags: ['attribute', 'text', 'css'],
+
+};
